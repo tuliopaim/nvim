da50efb024f13477cf865ff2d90a6a07e4f54dd6
tuliopaim <tutpaim@gmail.com>
2023-07-19 21:31:59 -0300

    remove ansible yml

--- lua/tuliopaim/remap.lua
+++ lua/tuliopaim/remap.lua
@@ -23,6 +23,9 @@ vim.api.nvim_set_keymap("n", "<C-Down>", ":resize -2<CR>", opts)
 vim.api.nvim_set_keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 vim.api.nvim_set_keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -33,13 +36,11 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>p", "\"_dP")
 
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
