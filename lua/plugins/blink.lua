50909849cd0202515b0736af4981ff750151ccc4
tuliopaim <tutpaim@gmail.com>
2025-08-08 18:36:23 -0300

    nvim updates

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -11,7 +11,25 @@ return {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = 'mono'
         },
-
+        signature = {
+          enabled = false,
+          trigger = {
+            enabled = true,
+          },
+        },
+        completion = {
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 500,
+            },
+            menu = {
+                auto_show = true,
+                draw = {
+                    treesitter = { "lsp" },
+                    columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
+                }
+            }
+        },
         sources = {
             default = { 'lsp', 'path', 'snippets', 'buffer', 'dadbod', 'avante'},
             providers = {
