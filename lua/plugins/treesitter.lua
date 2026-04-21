637e0cbdec47d42b5d1824951dfbb54e56ec6f37
tuliopaim <tutpaim@gmail.com>
2026-04-21 09:07:28 -0300

    update treesitter to `main`

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,35 +2,40 @@ return {
     {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
-        branch = 'master',
-        event = { "BufReadPost", "BufNewFile" }, -- Load when opening a file
+        branch = 'main',
+        lazy = false,
         config = function()
-            local config = require("nvim-treesitter.configs")
-            config.setup({
-                ensure_installed = {
-                    "regex",
-                    "bash",
-                    "c",
-                    "css",
-                    "html",
-                    "javascript",
-                    "json",
-                    "lua",
-                    "markdown",
-                    "markdown_inline",
-                    "prisma",
-                    "tsx",
-                    "typescript",
-                    "vim",
-                    "c_sharp",
-                },
-                auto_install = true,
-                highlight = { enable = true },
-                indent = { enable = true },
+            require('nvim-treesitter').install({
+                'regex',
+                'bash',
+                'c',
+                'css',
+                'html',
+                'javascript',
+                'json',
+                'lua',
+                'markdown',
+                'markdown_inline',
+                'nix',
+                'prisma',
+                'sql',
+                'tsx',
+                'typescript',
+                'vim',
+                'c_sharp',
+                'yaml'
             })
 
-            vim.treesitter.language.register("c_sharp", "csharp")
-        end
+            local group = vim.api.nvim_create_augroup('nvim-treesitter-migration', { clear = true })
+
+            vim.api.nvim_create_autocmd('FileType', {
+                group = group,
+                callback = function(args)
+                    pcall(vim.treesitter.start, args.buf)
+                    vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+            })
+        end,
     }
 }
 
